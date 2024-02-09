3e7eeef6b7a0f656d56ba083d813ae490d6d61e2
Kamal Sanjeewa <sanjeewacrack@gmail.com>
2024-02-10 00:56:47 +0800

    Refactor: updateBackStage function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -57,48 +57,23 @@ export class GildedRose {
   }
 
   private static updateBackStage(item: Item) {
-    if (item.name != ItemType.BRIE && item.name != ItemType.BACKSTAGE) {
-      if (item.quality > 0) {
-        if (item.name != ItemType.SULFURAS) {
-          item.quality = item.quality - 1;
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+
+      if(item.quality < 50){
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 1;
         }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == ItemType.BACKSTAGE) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (item.name != ItemType.SULFURAS) {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != ItemType.BRIE) {
-        if (item.name != ItemType.BACKSTAGE) {
-          if (item.quality > 0) {
-            if (item.name != ItemType.SULFURAS) {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
+        if (item.sellIn < 6) {
           item.quality = item.quality + 1;
         }
       }
+    }
+  
+    item.sellIn = item.sellIn - 1;
+    
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality;
     }
   }
 
