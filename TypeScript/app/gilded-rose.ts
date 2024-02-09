b1e34e523edec09fa9fd1618155f76aad01087fa
Kamal Sanjeewa <sanjeewacrack@gmail.com>
2024-02-10 00:52:24 +0800

    Refactor: updateBrie funtion

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -46,48 +46,13 @@ export class GildedRose {
   }
 
   private static updateBrie(item: Item) {
-    if (item.name != ItemType.BRIE && item.name != ItemType.BACKSTAGE) {
-      if (item.quality > 0) {
-        if (item.name != ItemType.SULFURAS) {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-        if (item.name == ItemType.BACKSTAGE) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-      }
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
     }
-    if (item.name != ItemType.SULFURAS) {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.sellIn < 0) {
-      if (item.name != ItemType.BRIE) {
-        if (item.name != ItemType.BACKSTAGE) {
-          if (item.quality > 0) {
-            if (item.name != ItemType.SULFURAS) {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+    item.sellIn = item.sellIn - 1;
+    
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality = item.quality + 1;
     }
   }
 
